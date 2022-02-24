54a43a6144ae256ce0c05fe94431cf4505cdec19
dhoangtu <dhoangtu@gmail.com>
2022-02-24 12:57:51 +0700

    xuat ban

--- sach/in-nhanh.bat
+++ sach/in-nhanh.bat
@@ -3,11 +3,9 @@
 
 set GEN=".\pdf-generated"
 
-D:\download\qpdf-10.1.0\bin\qpdf --empty --pages D:\01.lilypond\01.github\tuyentaphopca-lmkimlong\nhac\*.pdf -- nhac.pdf
+D:\download\qpdf-10.1.0\bin\qpdf --empty --pages D:\01.lilypond\01.github\bole-lmkimlong\nhac\*.pdf -- nhac.pdf
 pdflatex so-trang-chan-le.tex
 
-D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc.pdf bia-truoc-trong.pdf loi-ngo.pdf so-trang-chan-le.pdf blank-a4.pdf muc-luc.pdf muc-luc-phung-vu.pdf blank-a4.pdf bia-sau-trong.pdf bia-sau.pdf -- tuyentaphopca-lmkimlong.pdf
-
-D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc-trong.pdf loi-ngo.pdf so-trang-chan-le.pdf blank-a4.pdf muc-luc.pdf muc-luc-phung-vu.pdf blank-a4.pdf bia-sau-trong.pdf -- in-sach.pdf
+D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc-a5.pdf so-trang-chan-le.pdf bia-sau-a5.pdf -- bole-lmkimlong.pdf
 
 del /s /f /q %GEN% nhac.pdf *.aux *.log so-trang-chan-le.pdf
