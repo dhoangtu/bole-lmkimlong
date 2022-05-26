48aeaec7153f33dbaefa066cd39347721b6354e5
dhoangtu <dhoangtu@gmail.com>
2022-05-26 20:53:03 +0700

    xuat ban

--- sach/in-nhanh.bat
+++ sach/in-nhanh.bat
@@ -9,8 +9,8 @@ set AN_TANG="D:\01.lilypond\01.github\bole-lmkimlong\an-tang-cau-hon\sach\nhac-s
 D:\download\qpdf-10.1.0\bin\qpdf --empty --pages %BO_LE% %TUAN_THANH% %HON_PHOI% %AN_TANG% -- nhac.pdf
 pdflatex so-trang-chan-le.tex
 
-D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc-a5.pdf so-trang-chan-le.pdf bia-sau-trong-a5.pdf bia-sau-a5.pdf -- bole-lmkimlong.pdf
+D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc-a5.pdf so-trang-chan-le.pdf muc-luc.pdf bia-sau-trong-a5.pdf bia-sau-a5.pdf -- bole-lmkimlong.pdf
 
-D:\download\qpdf-10.1.0\bin\qpdf --empty --pages so-trang-chan-le.pdf bia-sau-trong-a5.pdf -- in-sach.pdf
+D:\download\qpdf-10.1.0\bin\qpdf --empty --pages so-trang-chan-le.pdf muc-luc.pdf bia-sau-trong-a5.pdf -- in-sach.pdf
 
 del /s /f /q %GEN% nhac.pdf *.aux *.log so-trang-chan-le.pdf
